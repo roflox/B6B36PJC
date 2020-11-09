c9fbd5c9c4c68c5d752c2d9bef2c77fed2b7a057
RofLox <wow.brog@gmail.com>
2020-11-09 13:09:25 +0100

    SYMCALC init

--- SYMCALC/expr_impl.cpp
+++ SYMCALC/expr_impl.cpp
@@ -3,8 +3,110 @@
 #include <iostream>
 #include <cmath>
 #include <limits>
+#include <utility>
 
 namespace exprs {
     // TODO
+
+
+    plus::plus(expr a, expr b) : a(std::move(a)), b(std::move(b)) {};
+    minus::minus(expr a, expr b) : a(std::move(a)), b(std::move(b)) {};
+    divide::divide(expr a, expr b) : a(std::move(a)), b(std::move(b)) {}
+    multiply::multiply(expr a, expr b) : a(std::move(a)), b(std::move(b)) {}
+
+    double plus::evaluate(const expr_base::variable_map_t &variables) const {
+        return 0;
+    }
+
+    expr plus::derive(const std::string &variable) const {
+        return expr();
+    }
+
+    expr plus::simplify() const {
+        return expr();
+    }
+
+    void plus::write(std::ostream &out, expr_base::WriteFormat fmt) const {
+
+    }
+
+    bool plus::equals(const expr_base &b) const {
+        return false;
+    }
+
+
+
+    number::number(double num) {
+
+    }
+
+    variable::variable(std::string var) {
+
+    }
+
+    double variable::evaluate(const expr_base::variable_map_t &variables) const {
+        return 0;
+    }
+
+    expr variable::derive(const std::string &variable) const {
+        return expr();
+    }
+
+    expr variable::simplify() const {
+        return expr();
+    }
+
+    void variable::write(std::ostream &out, expr_base::WriteFormat fmt) const {
+
+    }
+
+    bool variable::equals(const expr_base &b) const {
+        return false;
+    }
+
+    double minus::evaluate(const expr_base::variable_map_t &variables) const {
+        return 0;
+    }
+
+    expr minus::derive(const std::string &variable) const {
+        return expr();
+    }
+
+    expr minus::simplify() const {
+        return expr();
+    }
+
+    void minus::write(std::ostream &out, expr_base::WriteFormat fmt) const {
+
+    }
+
+    bool minus::equals(const expr_base &b) const {
+        return false;
+    }
+
+    minus::minus() {
+
+    }
+
+    double divide::evaluate(const expr_base::variable_map_t &variables) const {
+        return 0;
+    }
+
+    expr divide::derive(const std::string &variable) const {
+        return expr();
+    }
+
+    expr divide::simplify() const {
+        return expr();
+    }
+
+    void divide::write(std::ostream &out, expr_base::WriteFormat fmt) const {
+
+    }
+
+    bool divide::equals(const expr_base &b) const {
+        return false;
+    }
+
 
 } // namespace exprs
--- SYMCALC/expr_impl.hpp
+++ SYMCALC/expr_impl.hpp
@@ -5,4 +5,117 @@
 
 namespace exprs {
     // TODO
+    class variable : public expr_base {
+        friend class expr;
+
+    public:
+        explicit variable(std::string var);
+
+    private:
+        std::string var;
+    public:
+        double evaluate(const variable_map_t &variables) const override;
+
+        expr derive(const std::string &variable) const override;
+
+        expr simplify() const override;
+
+    private:
+        void write(std::ostream &out, WriteFormat fmt) const override;
+
+        bool equals(const expr_base &b) const override;
+    };
+
+    class number : public expr_base {
+        friend class expr;
+
+    public:
+        explicit number(double);
+
+        double evaluate(const variable_map_t &variables) const override;
+
+        expr derive(std::string const &variable) const override;
+
+        expr simplify() const override;
+
+    private:
+        void write(std::ostream &out, WriteFormat fmt) const override;
+
+        bool equals(const expr_base &b) const override;
+
+        double num;
+    };
+
+
+    class plus : public expr_base {
+        friend class expr;
+
+    public:
+        plus(expr, expr);
+
+        double evaluate(const variable_map_t &variables) const override;
+
+        expr derive(std::string const &variable) const override;
+
+        expr simplify() const override;
+
+    private:
+        expr a;
+        expr b;
+
+        void write(std::ostream &out, WriteFormat fmt) const override;
+
+        bool equals(const expr_base &b) const override;
+    };
+
+    class minus : public expr_base {
+    public:
+        minus(expr, expr);
+
+        double evaluate(const variable_map_t &variables) const override;
+
+        expr derive(const std::string &variable) const override;
+
+        expr simplify() const override;
+
+    private:
+        expr a;
+        expr b;
+
+        void write(std::ostream &out, WriteFormat fmt) const override;
+
+        bool equals(const expr_base &b) const override;
+    };
+
+    class multiply : public expr_base {
+
+    private:
+    public:
+        multiply(expr, expr);
+
+    private:
+        expr a;
+        expr b;
+    };
+
+    class divide : public expr_base {
+
+    public:
+        divide(expr a, expr b);
+
+        double evaluate(const variable_map_t &variables) const override;
+
+        expr derive(const std::string &variable) const override;
+
+        expr simplify() const override;
+
+    private:
+        void write(std::ostream &out, WriteFormat fmt) const override;
+
+        bool equals(const expr_base &b) const override;
+
+    private:
+        expr a;
+        expr b;
+    };
 }
